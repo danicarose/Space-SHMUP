34f27b2895ba83c6836f020336a7f63db2e99d99
danicarose <danica.calusin@gmail.com>
2017-11-27 13:41:00 -0500

    began writing Enemy script
    - one compiler error (Assertion failed: Removing Assets/_Prefabs because the asset does not exist)
    - enemy moves towards bottom of screen

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	public float speed = 10f;
+	public float fireRate = 0.3f;
+	public float health = 10;
+	public int score = 100;
+
+	public bool _______________;
+
+	public Bounds bounds;
+	public Vector3 boundsCenterOffset;
+
+	void Update(){
+		Move ();
+	}
+
+	public virtual void Move(){
+		Vector3 tempPos = pos;
+		tempPos.y -= speed * Time.deltaTime;
+		pos = tempPos;
+	}
+
+	public Vector3 pos{
+		get{
+			return(this.transform.position);
+		}set{
+			this.transform.position = value;
+		}
+	}
+
+}
